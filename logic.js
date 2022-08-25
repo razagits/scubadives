312d43b004a3d0a70f7b8eb3711bfa602049912d
razagits <aliraza.glorious14@gmail.com>
2022-08-25 16:11:12 +0500

    scuba

---
+++ logic.js
@@ -0,0 +1,31 @@
+const navToggler = document.querySelector(".nav-toggler");
+navToggler.addEventListener("click", navToggle);
+
+function navToggle() {
+   navToggler.classList.toggle("active");
+   const nav = document.querySelector(".nav");
+   nav.classList.toggle("open");
+   if(nav.classList.contains("open")){
+       nav.style.maxHeight = nav.scrollHeight + "px";
+   }
+   else{
+       nav.removeAttribute("style");
+   }
+} 
+// When the user scrolls the page, execute myFunction
+window.onscroll = function() {myFunction()};
+
+// Get the navbar
+var navbar = document.getElementById("navbar");
+
+// Get the offset position of the navbar
+var sticky = navbar.offsetTop;
+
+// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky")
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}
